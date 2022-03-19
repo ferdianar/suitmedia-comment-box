3e33431adbc5a45d106d33ea8ccc69b1c5cba12a
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-19 19:21:50 +0700

    import comments json

--- apis/index.ts
+++ apis/index.ts
@@ -1,2 +1,27 @@
-//apis inside
-export const apis = () => { }
+export const TopicList = [
+    {
+        id: 1,
+        title: "Bersihkan laptop dari butiran debu",
+        created_at: "22 Desember 2021"
+    },
+    {
+        id: 2,
+        title: "Cara akses website menggunakan koneksi openVPN",
+        created_at: "26 Oktober 2021"
+    },
+    {
+        id: 3,
+        title: "Batas aman overclock PC rakitan",
+        created_at: "12 September 2021"
+    },
+    {
+        id: 4,
+        title: "Cara mengetahui akun Facebook di-hack melalui aplikasi",
+        created_at: "10 September 2021"
+    },
+    {
+        id: 5,
+        title: "Tutorial: langkah-langkah mencegah website untuk track user",
+        created_at: "18 Agustus 2021"
+    }
+]
